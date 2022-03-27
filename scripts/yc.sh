0f223d79bd65b8652867f68b6ad96742151ff22f
Alexey Kirinyuk <a.kirinyuk@icloud.com>
2022-03-27 20:35:17 +0300

    set up internal database for stg

--- scripts/yc.sh
+++ scripts/yc.sh
@@ -2,4 +2,12 @@
 sudo docker build -t otus-sn-api -f ./src/Otus.SocialNetwork/Dockerfile .
 
 # run
-sudo docker run -p 80:80 -e ASPNETCORE_ENVIRONMENT=Staging -d otus-sn-api
+MIGRATION_PATH=$(pwd)/../src/Persistence/Otus.SocialNetwork.Persistence/Migrations
+sudo docker run --name otus-social-network-db -p 3306:3306 \
+    -v $MIGRATION_PATH:/docker-entrypoint-initdb.d/ \
+    -e "MYSQL_DATABASE=otus_social_network" \
+    -e "MYSQL_ROOT_PASSWORD=mysql" \
+    -d \
+    mysql
+    
+sudo docker run -p 80:80 -e ASPNETCORE_ENVIRONMENT=Development -d otus-sn-api
