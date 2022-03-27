901ef8adb660f00ce7ff71920d13911e07a70475
Alexey Kirinyuk <a.kirinyuk@icloud.com>
2022-03-27 19:47:11 +0300

    fixing stagin env

--- scripts/yc.sh
+++ scripts/yc.sh
@@ -1,19 +1,5 @@
-ssh akirinyuk@51.250.107.91
+# build container
+sudo docker build -t otus-sn-api -f ./src/Otus.SocialNetwork/Dockerfile .
 
-scp -r ./Otus.SocialNetwork akirinyuk@51.250.107.91:/home/akirinyuk/otus-sn/
-
-# build container and push
-docker build -t cr.yandex/crplr7um5r6f5265e93d/otus-sn-api:test-0 \
-  -f ./src/Otus.SocialNetwork/Dockerfile --push .
-  
-# publish
-docker push cr.yandex/crplr7um5r6f5265e93d/otus-sn-api:v1.0.1
-
-# pull
-sudo docker pull --platform linux/arm64 cr.yandex/crplr7um5r6f5265e93d/otus-sn-api:test-0
-
-sudo docker run -p 80:80 -e ASPNETCORE_ENVIRONMENT=Development \
- --platform linux/arm64 \
- cr.yandex/crplr7um5r6f5265e93d/otus-sn-api:test-0
- 
- sudo docker run cr.yandex/crplr7um5r6f5265e93d/otus-sn-api:test-0
+# run
+sudo docker run -p 80:80 -e ASPNETCORE_ENVIRONMENT=Staging -d otus-sn-api
